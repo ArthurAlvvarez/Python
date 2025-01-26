792b1c280e344ad2fd19a049a276585918c097e9
ArthurAlvvarez <arturo.alvarez.gomez@colegiolitterator.com>
2025-01-26 18:51:29 +0100

    69, 73 y 74

---
+++ 73_sumaValores.py
@@ -0,0 +1,11 @@
+personas = {
+    "Arthur":25,
+    "Emi":30,
+    "Alvarito":20,
+    "Pacheco":10
+}
+resultado = 0
+
+for i in personas:
+    resultado += personas[i]
+print(resultado)
---
+++ 74_invertirDiccionario.py
@@ -0,0 +1,9 @@
+ciudades = {
+    "Madrid": 28300,
+    "Barcelona": 28953
+}
+ciudades_invertidas = {}
+
+for i in ciudades:
+    ciudades_invertidas[ciudades[i]] = i
+print(ciudades_invertidas)
